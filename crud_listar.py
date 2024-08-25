9ef945d1b682486d0b3912b149d910c5eac76dfa
Ezequiel-leandro-jr <ezequiel.leandro.jr@hotmail.com>
2024-08-24 22:11:05 -0300

    crud_listar.py atualizado c/ EDITAR, DELETAR, SAIR

--- crud_listar.py
+++ crud_listar.py
@@ -1,8 +1,11 @@
+from crud_editar import editar
+from crud_deletar import deletar
 import time
 
 def listar(portfolio):
     while True:
         i = 1
+        soma = 0
         
         for veiculo in portfolio:
             print(f'''
@@ -12,12 +15,24 @@ def listar(portfolio):
     ANO: {veiculo.ano_fabricacao} | COR: {veiculo.cor} | PORTAS: {veiculo.portas} | COMBUSTIVEL: {veiculo.combustivel}
     ESTADO: {veiculo.conservacao} | QUILOMETRAGEM: {veiculo.quilometragem:.2f}Km | PRECO: R${veiculo.preco:.2f}
     STATUS: {veiculo.status}
-    ______________________________________________________________________________________________________________________''')
+    ''')
             i += 1
-        
-        n = input('VOLTAR AO MENU [1]\n>>> ')
-        if n != '1':
-            print('ERRO: opcao invalida!')
-            time.sleep(1)
-        else:
-            break
+            soma += 1
+            
+        print(f'TOTAL DE VEICULOS: {soma}\n______________________________________________________________________________________________________________________')
+        while True:
+            n = input('\nEDITAR VEICULO [1]\nDELETAR VEICULO [2]\nVOLTAR AO MENU [3]\n>>> ')
+            if n == '1':
+                placa = input('PLACA: ')
+                editar(portfolio, placa)
+                return
+            elif n == '2':
+                placa = input('PLACA: ')
+                deletar(portfolio, placa)
+                return
+                
+            elif n == '3':
+                return
+            else:
+                print('ERRO: opcao invalida!')
+                time.sleep(1)
