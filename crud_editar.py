843ea77d39c16b3b8db637ebbb32e19b05558e5c
Ezequiel-leandro-jr <ezequiel.leandro.jr@hotmail.com>
2024-08-24 23:49:38 -0300

    time.sleep de crud_editar bem sucedido editado

--- crud_editar.py
+++ crud_editar.py
@@ -6,7 +6,7 @@ import time
 def editar(portfolio, placa):
     while True:
         veiculo = funcao_busca(portfolio, placa)
-        antigo_veiculo = veiculo
+
         if veiculo:
             indice = portfolio.index(veiculo)
             while True:
@@ -18,6 +18,9 @@ def editar(portfolio, placa):
                         alterar = input('SELECIONE O CAMPO:\n1. Placa\n2. Tipo\n3. Marca\n4. Modelo\n5. Cor\n6. Ano de fabricação\n7. Portas\n8. Combustível\n9. Conservação\n10. Quilometragem\n11. Preço\n12. Status\n\n0. Voltar\n>> ')
                         if alterar == '1':
                             veiculo.placa = input('PLACA: ')
+                            portfolio[indice] = veiculo
+                            print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                            time.sleep(2)
                         elif alterar == '2':
                             while True:
                                 tipo = input('''
@@ -36,22 +39,42 @@ OP: ''')
                                 if tipo in ['1', '2', '3', '4', '5', '6', '7']:
                                     tipos = ['Camioneta', 'Caminhonete', 'Caminhão', 'Carro', 'Carreta', 'Motocicleta', 'Outro']
                                     veiculo.tipo = tipos[int(tipo) - 1]
+                                    portfolio[indice] = veiculo
+                                    print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                                    time.sleep(2)
                                     break
                                 elif tipo == '0':
-                                    return
+                                    print('EDIÇÃO CANCELADA!')
+                                    time.sleep(2)
+                                    break
                                 else:
                                     print('ERRO: Opção incorreta! Tente novamente!')
                                     time.sleep(1)
                         elif alterar == '3':
                             veiculo.marca = input('MARCA: ')
+                            portfolio[indice] = veiculo
+                            print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                            time.sleep(2)
                         elif alterar == '4':
                             veiculo.modelo = input('MODELO: ')
+                            portfolio[indice] = veiculo
+                            print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                            time.sleep(2)
                         elif alterar == '5':
                             veiculo.cor = input('COR: ')
+                            portfolio[indice] = veiculo
+                            print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                            time.sleep(2)
                         elif alterar == '6':
                             veiculo.ano_fabricacao = input('ANO: ')
+                            portfolio[indice] = veiculo
+                            print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                            time.sleep(2)
                         elif alterar == '7':
                             veiculo.portas = int(input('PORTAS: '))
+                            portfolio[indice] = veiculo
+                            print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                            time.sleep(2)
                         elif alterar == '8':
                             while True:
                                 combustivel = input('''
@@ -70,9 +93,14 @@ OP: ''')
                                 if combustivel in ['1', '2', '3', '4', '5', '6', '7']:
                                     combustiveis = ['Gasolina', 'GLP', 'Etanol', 'GNV', 'Elétrico', 'Híbrido', 'Outro']
                                     veiculo.combustivel = combustiveis[int(combustivel) - 1]
+                                    portfolio[indice] = veiculo
+                                    print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                                    time.sleep(2)
                                     break
                                 elif combustivel == '0':
-                                    return
+                                    print('EDIÇÃO CANCELADA!')
+                                    time.sleep(2)
+                                    break
                                 else:
                                     print('ERRO: Opção incorreta! Tente novamente!')
                                     time.sleep(1)
@@ -81,45 +109,50 @@ OP: ''')
                                 conservacao = input('ESTADO DE CONSERVAÇÃO:\n1. Novo\n2. Seminovo\n\n\n0. Voltar\n----------------\nOP: ')
                                 if conservacao in ['1', '2']:
                                     veiculo.conservacao = 'Novo' if conservacao == '1' else 'Seminovo'
+                                    portfolio[indice] = veiculo
+                                    print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                                    time.sleep(2)
                                     break
                                 elif conservacao == '0':
-                                    return
+                                    print('EDIÇÃO CANCELADA!')
+                                    time.sleep(2)
+                                    break
                                 else:
                                     print('ERRO: Opção incorreta! Tente novamente!')
                                     time.sleep(1)
                         elif alterar == '10':
                             veiculo.quilometragem = float(input('QUILOMETRAGEM (KM): '))
+                            portfolio[indice] = veiculo
+                            print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                            time.sleep(2)
                         elif alterar == '11':
                             veiculo.preco = float(input('PREÇO (R$): '))
+                            portfolio[indice] = veiculo
+                            print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                            time.sleep(2)
                         elif alterar == '12':
                             while True:
                                 status = input('STATUS:\n1. À venda\n2. Reservado\n3. Vendido\n4. Indisponível\n\n0. Voltar\n----------------\nOP: ')
                                 if status in ['1', '2', '3', '4']:
                                     status_opcoes = ['À venda', 'Reservado', 'Vendido', 'Indisponível']
                                     veiculo.status = status_opcoes[int(status) - 1]
+                                    portfolio[indice] = veiculo
+                                    print('\nEDIÇÃO REALIZADA COM SUCESSO!')
+                                    time.sleep(2)
                                     break
                                 elif status == '0':
-                                    return
+                                    print('EDIÇÃO CANCELADA!')
+                                    time.sleep(2)
+                                    break
                                 else:
                                     print('ERRO: Opção inválida!')
                                     time.sleep(1)
                         elif alterar == '0':
-                            return
+                            break
                         else:
                             print('ERRO: Opção inválida!')
                             time.sleep(1)
-                            continue
-
-                        
-                        if veiculo == portfolio[indice]:
-                            print('EDIÇÃO CANCELADA!')
-                            time.sleep(2)
-                            break
-                        else:
-                            portfolio[indice] = veiculo
-                            print('EDIÇÃO REALIZADA COM SUCESSO!')
-                            time.sleep(2)
-                            break
+                            continue           
                             
                 elif op == '2':
                     placa = input('PLACA: ')
