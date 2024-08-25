b7e3cb8772443e72df5dbf0e5c6dd04713d076e6
Ezequiel-leandro-jr <ezequiel.leandro.jr@hotmail.com>
2024-08-25 00:18:26 -0300

    cabecalho e os.system DELETAR

--- crud_deletar.py
+++ crud_deletar.py
@@ -2,51 +2,73 @@ from funcao_busca import funcao_busca
 from funcao_exibir import funcao_exibir
 from cabecalhos import titulo_automarket, titulo_buscar, titulo_deletar, titulo_editar, titulo_registrar
 import time
+import os
 
 def deletar(portfolio, placa):
     while True:
         veiculo = funcao_busca(portfolio, placa)
         if veiculo:
-            funcao_exibir(veiculo)
             n = '4'
             while n == '4':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                titulo_automarket()
+                titulo_deletar()
+                funcao_exibir(veiculo)
                 n = input('DELETAR [1]\nNOVA BUSCA [2]\nVOLTAR AO MENU [3]\n>>> ')
                 match n:
                     case '1':
                         portfolio.remove(veiculo)
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        titulo_automarket()
+                        titulo_deletar()
                         print('VEÍCULO DELETADO COM SUCESSO!')
                         time.sleep(2)
                         n = '0'
                         while n == '0':
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            titulo_automarket()
+                            titulo_deletar()
                             n = input('DELETAR OUTRO VEÍCULO [1]\nVOLTAR AO MENU [2]')
                             if n == '1':
+                                os.system('cls' if os.name == 'nt' else 'clear')
+                                titulo_automarket()
+                                titulo_deletar()
                                 placa = input('PLACA: ')
                             elif n == '2':
                                 return
                             else:
-                                print('Erro: opção inválida!')
+                                print('\nERRO: opção inválida!')
                                 n = '0'
                                 time.sleep(1)        
                     case '2':
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        titulo_automarket()
+                        titulo_deletar()
                         placa = input('PLACA: ')
                         n = '5'
                     case '3':
                         n = '5'
                         return
                     case _:
-                        print('ERRO: opção inválida!')
+                        print('\nERRO: opção inválida!')
                         n = '4'
                         time.sleep(1)
         else:
-            print('ERRO: Veículo não encontrado!')
             while True:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                titulo_automarket()
+                titulo_deletar()
+                print('\nERRO: Veículo não encontrado!')
                 n = input('NOVA DELEÇÃO [1]\nVOLTAR AO MENU [2]\n>> ')
                 if n == '1':
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    titulo_automarket()
+                    titulo_deletar()
                     placa = input('PLACA: ')
                     break  
                 elif n == '2':
                     return 
                 else:
-                    print('ERRO: Opção inválida!')
+                    print('\nERRO: Opção inválida!')
                     time.sleep(1)        
             
